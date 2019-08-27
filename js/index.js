c3583d47d1c6990c54e59bb3045f67dc8447016c
zhangxq2019 <46281242+zhangxq2019@users.noreply.github.com>
2019-08-27 11:46:35 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -69,8 +69,8 @@ function getMusicList(callback){
 var xhr = new XMLHttpRequest();
 xhr.open('GET','/music.json',true);
 xhr.onload = function(){
-    if((xhr.status >=200 && xhr.status < 300) || xhr.status ===304){
-    callback(JSON.parse(this.responseText));
+    if((xhr.status >=200 && xhr.status < 300) || xhr.status == 304){
+    callback(JSON.parse(xhr.responseText));
     // window.musicList = JSON.parse(this.responseText);
 } else {
     console.log('获取数据失败');
